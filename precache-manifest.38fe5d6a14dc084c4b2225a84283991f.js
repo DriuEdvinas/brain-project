7ebf19aeb6e097307a59784a864aee64a6db42be
DriuEdvinas <35631124+DriuEdvinas@users.noreply.github.com>
2020-07-26 12:37:03 +0300

    Updates

---
+++ precache-manifest.38fe5d6a14dc084c4b2225a84283991f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0dacd57cda7f9264bd6fd1337d35d1e",
+    "url": "/brain-project/index.html"
+  },
+  {
+    "revision": "f596a5dcd9572585bdca",
+    "url": "/brain-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "d00f9e0f39609b396b07",
+    "url": "/brain-project/static/css/main.9cc5577e.chunk.css"
+  },
+  {
+    "revision": "f596a5dcd9572585bdca",
+    "url": "/brain-project/static/js/2.457c6c23.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/brain-project/static/js/2.457c6c23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d00f9e0f39609b396b07",
+    "url": "/brain-project/static/js/main.b59a1e8a.chunk.js"
+  },
+  {
+    "revision": "652888e9255d27a599cd",
+    "url": "/brain-project/static/js/runtime-main.5918776d.js"
+  },
+  {
+    "revision": "f574e83c858ff07aafdeafae016dc221",
+    "url": "/brain-project/static/media/LogoW.f574e83c.png"
+  }
+]);
